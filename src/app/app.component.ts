6f5051f25f0fe74f5da4ba53c9490ef55385bcb3
noami jofen <noamijofen@gmail.com>
2017-11-30 16:26:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestoreDocument,AngularFirestoreCollection} from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public name:string;
+  public lname:string;
+  private itemDoc: AngularFirestoreDocument<any>;
+  private col:AngularFirestoreCollection<any>;
+  private users;
+   constructor(private afs: AngularFirestore) {
+   this.itemDoc =this.afs.doc("users/1");
+   this.col=this.afs.collection("users");
+   this.col.valueChanges().subscribe(res=>{
+     console.log(res);
+     this.users=res;
+   })
+
+  /*let item=this.itemDoc.valueChanges().subscribe(res=>{
+      //console.log(res);
+    this.name=res.name;
+    this.lname=res.lname;
+     // this.users=res;
+    });*/
+  }
+   send(){
+    this.col.add({name:this.name,lname:this.lname}).then(res=>{
+    //  alert(res);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+export const environment={
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB0h3SAyF6bJj9Xhv5UtOGlwkGhgH0efw0",
+    authDomain: "myapp-7bd05.firebaseapp.com",
+    databaseURL: "https://myapp-7bd05.firebaseio.com",
+    projectId: "myapp-7bd05",
+    storageBucket: "myapp-7bd05.appspot.com",
+    messagingSenderId: "740158676491"
+  }
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   AngularFireModule.initializeApp(environment.firebase),
+   AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
